a13dd633f989c1b4a232661cc6eeace08fa8e83e
Drieschf <fraukevondendriesch@Fraukes-Air.fritz.box>
2019-05-27 19:49:57 +0200

    exercise 3 final

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # Exercise 3
+
 from pathlib import Path
+import utils as ut
+import csv
+
 # import functions from utils here
 
 data_dir = Path("data")
@@ -7,18 +11,31 @@ output_dir = Path("solution")
 
 # 1. Contstruct the path to the text file in the data directory using the `pathlib` module [2P]
 
+cars_dir = Path(data_dir, "cars.txt")
 
 # 2. Read the text file [2P]
-
+    
+cars=open(str(cars_dir), "r").read()
 
 # 3. Count the occurences of each item in the text file [2P]
 
+clist = list(cars.split("\n"))
+occur = ut.countelem(clist)
 
 # 4. Using `pathlib` check if a directory with name `solution` exists and if not create it [2P]
 
+ut.dir_check(output_dir)
 
 # 5. Write the counts to the file `counts.csv` in the `solution` directory in the format (first line is the header): [2P]
 #    item, count
 #    item_name_1, item_count_1
 #    item_name_2, item_count_2
 #    ...
+
+counts = output_dir/"counts.csv"
+
+with open(counts, "w") as file:
+    writer = csv.DictWriter(file, fieldnames=["item", "count"]) 
+    writer.writeheader()
+    for key in occur.keys():
+        file.write("%s,%s\n"%(key,occur[key])) 
--- utils.py
+++ utils.py
@@ -2,3 +2,47 @@
 
 # Use this file for all functions you create and might want to reuse later.
 # Import them in the `main.py` script
+
+#function for counting occurence of elements in list
+def countelem(l:list):
+    
+    """
+    loop going through every list element and counts occurence: sets 1 for new 
+    elements and adds +1 for already existing elements 
+    
+    Parameters
+    ---
+    l : list with elements to count
+    x : elements of l
+    
+    Returns
+    ---
+    elem : list with counted occurences of elements from l
+    
+    """
+    
+    elem = {}
+    for x in l:
+        if x in elem.keys():
+            elem[x] += 1
+        else:
+            elem[x] = 1
+    return(elem)
+
+#function directory check with pathlib
+
+def dir_check(pth):
+    
+    """
+    checks if directory exists, will inform user if it already exists and create directory otherwise
+    
+    Parameters
+    ---
+    pth : directory to check for  
+    """
+    
+    if pth.exists() == False :
+        print("Sorry, directory \"{}\" is not existing yet, will create now".format(pth))
+        pth.mkdir()    
+    else:
+        print("Directory \"{}\" already exists".format(pth.exists))  
